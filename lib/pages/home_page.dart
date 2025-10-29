a5f7a0870016f5b09fe3a97397e8a88e1c4d9ee4
bentleyw117 <theofficialbentley117@gmail.com>
2025-10-29 11:35:36 -0500

    New file size.dart contains sizing for window variable.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:my_portfolio_website/constants/colors.dart';
 import 'package:my_portfolio_website/constants/nav_items.dart';
+import 'package:my_portfolio_website/constants/size.dart';
 import 'package:my_portfolio_website/styles/style.dart';
 import 'package:my_portfolio_website/widgets/drawer_mobile.dart';
 import 'package:my_portfolio_website/widgets/header_desktop.dart';
@@ -25,12 +26,12 @@ class _HomePageState extends State<HomePage> {
         return Scaffold(
           key: scaffoldKey,
           backgroundColor: CustomColor.scaffoldBg,
-          endDrawer: const DrawerMobile(),
+          endDrawer: constraints.maxWidth >= kMinDesktopWidth ? null: DrawerMobile(),
           body: ListView(
             scrollDirection: Axis.vertical,
             children: [
               // MAIN
-              if(constraints.maxWidth >= 600.0)
+              if(constraints.maxWidth >= kMinDesktopWidth)
               const HeaderDesktop() else
               HeaderMobile(
                 onLogoTap: () {},
