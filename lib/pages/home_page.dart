2d219cd004ded97e294c9b13e5d20b1f36f58076
bentleyw117 <theofficialbentley117@gmail.com>
2025-10-28 21:24:10 -0500

    Seperated desktop and mobile headers, making site logo and a box style reusable.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio_website/constants/colors.dart';
 import 'package:my_portfolio_website/constants/nav_items.dart';
+import 'package:my_portfolio_website/styles/style.dart';
+import 'package:my_portfolio_website/widgets/header_desktop.dart';
+import 'package:my_portfolio_website/widgets/header_mobile.dart';
+import 'package:my_portfolio_website/widgets/site_logo.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,54 +22,10 @@ class _HomePageState extends State<HomePage> {
         scrollDirection: Axis.vertical,
         children: [
           // MAIN
-          Container(
-            height: 50,
-            margin: EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 20
-            ),
-            width: double.maxFinite,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Colors.transparent,
-                CustomColor.bgLight1
-                ],
-              ),
-              borderRadius: BorderRadius.circular(100),
-            ),
-            child: Row(
-              children: [
-                  ShaderMask(
-                    shaderCallback: (Rect bounds) {
-                      return CustomColor.logoGradient.createShader(Offset.zero & bounds.size);
-                    },
-                    child: Text(
-                    'BW',
-                    style: TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Spacer(),
-                for (int i = 0; i < navTitles.length; i++)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: TextButton(
-                      onPressed: (){},
-                      child: Text(
-                        navTitles[i],
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          color: CustomColor.whitePrimary,
-                        ),
-                      )
-                    ),
-                  )
-              ],
-            ),
+          //HeaderDesktop(),
+          HeaderMobile(
+            onLogoTap: () {},
+            onMenuTap: () {},
           ),
           // SKILLS
           Container(
---
+++ lib/widgets/header_desktop.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio_website/constants/colors.dart';
+import 'package:my_portfolio_website/constants/nav_items.dart';
+import 'package:my_portfolio_website/styles/style.dart';
+import 'package:my_portfolio_website/widgets/site_logo.dart';
+
+class HeaderDesktop extends StatelessWidget {
+  const HeaderDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+            height: 50,
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 20
+            ),
+            width: double.maxFinite,
+            decoration: kHeaderDecoration,
+            child: Row(
+              children: [
+                SiteLogo(
+                  onTap: () {},
+                ),
+                Spacer(),
+                for (int i = 0; i < navTitles.length; i++)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: TextButton(
+                      onPressed: (){},
+                      child: Text(
+                        navTitles[i],
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: CustomColor.whitePrimary,
+                        ),
+                      )
+                    ),
+                  )
+              ],
+            ),
+          );
+  }
+}
